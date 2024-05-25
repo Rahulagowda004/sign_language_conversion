152d330728e8daebcf73b4e9749bc7787a34d7cb
rahulagowda004 <rahulgowda277@gmail.com>
2024-05-25 21:07:37 +0530

    the research part is working

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from sign_language_conversion import logger
 from sign_language_conversion.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from sign_language_conversion.pipeline.stage_02_prepare_dataset_pickle import PreparedatasetPipeline
 from sign_language_conversion.pipeline.stage_03_training_model import TrainingModel
 from sign_language_conversion.pipeline.stage_04_model_evaluation import ModelEvaluation
 
@@ -13,36 +12,25 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-STAGE_NAME = "Prepare base model"
-try: 
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   prepare_base_model = PreparedatasetPipeline()
-   prepare_base_model.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
      
-STAGE_NAME = "Training the Model"
-try: 
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   prepare_base_model = TrainingModel()
-   prepare_base_model.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+# STAGE_NAME = "Training the Model"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    prepare_base_model = TrainingModel()
+#    prepare_base_model.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
      
-STAGE_NAME = "Evaluation of Model"
-try:
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   obj = ModelEvaluation()
-   obj.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-   logger.exception(e)
-   raise e
+# STAGE_NAME = "Evaluation of Model"
+# try:
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    obj = ModelEvaluation()
+#    obj.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#    logger.exception(e)
+#    raise e
