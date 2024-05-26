9f4cb873e14cb292fbc3a6152dd0d8b754929c19
rahulagowda004 <rahulgowda277@gmail.com>
2024-05-26 09:39:23 +0530

    predict is working.
    THE MAIN COMMIT.

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from sign_language_conversion import logger
 from sign_language_conversion.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from sign_language_conversion.pipeline.stage_02_training_model import TrainingModel
 from sign_language_conversion.pipeline.stage_03_model_evaluation import ModelEvaluation
+from sign_language_conversion.pipeline.prediction import HandGestureRecognition
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
      
-# STAGE_NAME = "Training the Model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = TrainingModel()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Training the Model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = TrainingModel()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
      
 STAGE_NAME = "Evaluation of Model"
 try:
@@ -34,3 +35,14 @@ try:
 except Exception as e:
    logger.exception(e)
    raise e
+
+STAGE_NAME = "Prediction of Model"
+try:
+    logger.info("*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = HandGestureRecognition()
+    obj.recognize_gesture()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
