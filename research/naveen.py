62bca39bc8c6b352fa5e42358e3655565e18ddf8
rahulagowda004 <rahulgowda277@gmail.com>
2024-06-03 16:41:49 +0530

    kinda working

--- research/naveen.py
+++ research/naveen.py
@@ -1,3 +1,6 @@
+import os
+os.chdir("../")
+
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='google.protobuf.symbol_database')
 
@@ -24,7 +27,7 @@ class HandGestureRecognition:
             0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I',
             9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q',
             17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y',
-            25: 'Z', 26: 'del', 27: ' ', 28: 'wait'
+            25: 'Z', 26: 'del', 27: " ", 28: 'wait'
         }
         
         self.output_file = open('output.txt', 'w')  # Open file to save outputs
@@ -99,7 +102,7 @@ class HandGestureRecognition:
             self.output_file.close()  # Close the output file
             cv2.destroyAllWindows()
             cv2.waitKey(1)
-
+            
 if __name__ == "__main__":
     recognizer = HandGestureRecognition()
     recognizer.recognize_gesture()
